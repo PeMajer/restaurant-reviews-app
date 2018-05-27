1315b24a3e0dad79434af93a72a8888e1e88b9df
Petr Majer <majer.petr@gmail.com>
2018-05-27 15:12:55 +0200

    refactor: rewrite function to ES6

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 * @description register Service Worker
 */
 
-(function () {
+( () => {
     if (!navigator.serviceWorker) return;
     navigator.serviceWorker.register('/sw.js')
         .then(function(){
